ec8bf2dd02dbe093fc16a6312ef89a7657dcd230
sss-szk <kmpkpcbszk724@gmail.com>
2024-04-20 15:20:45 +0900

    Squashed commit of the following:
    commit 1bf0d5cacbbdbd3b9f4e89789ec8ad46b2f8d835
    Author: sss-szk <kmpkpcbszk724@gmail.com>
    Date:   Tue Jan 23 00:23:35 2024 +0900
    
        リリース用ファイル作成
    
    commit eaf3f2675a8a54ae3f3b0b78ed968ec6f3f2d6db
    Author: sss-szk <kmpkpcbszk724@gmail.com>
    Date:   Tue Jan 23 00:22:46 2024 +0900
    
        保存ボタン押下時のメッセージを追加
    
    commit 010818e05b0258b661380b2acb2a09f024f50694
    Author: sss-szk <kmpkpcbszk724@gmail.com>
    Date:   Mon Jan 22 23:50:55 2024 +0900
    
        リリース用ファイル作成
    
    commit 0b4db61c70896db91ad7cdbad484337b75598f7f
    Author: sss-szk <kmpkpcbszk724@gmail.com>
    Date:   Mon Jan 22 23:49:01 2024 +0900
    
        フォームの初期値をクッキーに保存できるように修正
    
    commit 556142fb8efedfd2deb659a969eaf4211a96a00d
    Author: sss-szk <kmpkpcbszk724@gmail.com>
    Date:   Mon Jan 22 22:58:46 2024 +0900
    
        HispeedCalculatorのBPMとGreenCalculatorのBPMが連動するように修正

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,68 @@
 <template>
   <h1>ハイスピを固定しつつ白数字を変えたときの緑数字を出すやつ</h1>
   <label class="subtitle">現在のハイスピ設定</label>
-  <HispeedCalculator></HispeedCalculator>
+  <HispeedCalculator v-model:bpm="bpm" v-model:white="white" v-model:green="green"></HispeedCalculator>
+  <button type="button" class="btn btn-primary" @click="saveToCookies">Save</button>
+  <div v-if="isMessageVisible">
+    <br />
+    <p>初期値を保存しました</p>
+    <p>BPM:{{ this.bpm }} 白数字:{{ this.white }} 緑数字:{{ this.green }} </p>
+  </div>
+  <br />
   <label class="subtitle">新しいハイスピ設定</label>
-  <GreenCalculator></GreenCalculator>
+  <GreenCalculator v-model:bpm="bpm"></GreenCalculator>
 </template>
 
 <script>
 import HispeedCalculator from './components/HispeedCalculator.vue'
 import GreenCalculator from './components/GreenCalculator.vue';
+import Cookies from 'js-cookie';
 
 export default {
   name: 'App',
   components: {
     HispeedCalculator,
     GreenCalculator
+  },
+  data: function(){
+    return {
+      // フォーム初期値
+      bpm: 187,
+      white: 455,
+      green: 290,
+      isMessageVisible: false,
+    }
+  },
+  methods: {
+    saveToCookies() {
+      Cookies.set('bpm', this.bpm, { expires: 30 });
+      Cookies.set('white', this.white, { expires: 30 });
+      Cookies.set('green', this.green, { expires: 30 });
+      this.showMessage();
+    },
+    loadFromCookies() {
+      const savedBpm = Cookies.get('bpm');
+      const savedWhite = Cookies.get('white');
+      const savedGreen = Cookies.get('green');
+
+      if (savedBpm !== undefined) this.bpm = parseInt(savedBpm);
+      if (savedWhite !== undefined) this.white = parseInt(savedWhite);
+      if (savedGreen !== undefined) this.green = parseInt(savedGreen);
+    },
+    showMessage() {
+      this.isMessageVisible = true;
+    },
+    closeMessage() {
+      if (this.isMessageVisible) this.isMessageVisible = false;
+    },
+  },
+  watch: {
+    bpm: 'closeMessage',
+    white: 'closeMessage',
+    green: 'closeMessage',
+  },
+  mounted(){
+    this.loadFromCookies();
   }
 }
 </script>
--- src/components/HispeedCalculator.vue
+++ src/components/HispeedCalculator.vue
@@ -5,21 +5,21 @@
       <div class="row justify-content-center">
         <div class="col-md-2 mb-3">
           <label for="bpm">BPM:</label>
-          <input type="number" class="form-control" id="bpm" v-model="bpm" />
+          <input type="number" class="form-control" id="bpm" v-model="localBpm" />
         </div>
       </div>
 
       <div class="row justify-content-center">
         <div class="col-md-2 mb-3">
           <label for="white">白数字:</label>
-          <input type="number" class="form-control" id="white" v-model="white" />
+          <input type="number" class="form-control" id="white" v-model="localWhite" />
         </div>
       </div>
 
       <div class="row justify-content-center">
         <div class="col-md-2 mb-3">
           <label for="green">緑数字:</label>
-          <input type="number" class="form-control" id="green" v-model="green" />
+          <input type="number" class="form-control" id="green" v-model="localGreen" />
         </div>
       </div>
 
@@ -35,14 +35,59 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue';
 export default {
+  props: {
+    bpm: {
+      type: Number,
+      required: true,
+    },
+    white: {
+      type: Number,
+      required: true,
+    },
+    green: {
+      type: Number,
+      required: true,
+    },
+  },
   // HTMLで使う変数
   data: function () {
     return {
-      bpm: 187,
-      white: 455,
-      green: 290,
       hispeed: 0
+    };
+  },
+  setup(props, { emit }) {
+    // refを使用してローカルなリアクティブな変数 localBpm を定義し、props.bpm の初期値で初期化
+    const localBpm = ref(props.bpm);
+    const localWhite = ref(props.white);
+    const localGreen = ref(props.green);
+
+    // localBpm の変更を検知して、emit('update:bpm', newBpm) で親コンポーネントに通知
+    watch(localBpm, (newBpm) => {
+      emit('update:bpm', newBpm);
+    });
+    watch(localWhite, (newWhite) => {
+      emit('update:white', newWhite);
+    });
+    watch(localGreen, (newGreen) => {
+      emit('update:green', newGreen);
+    });
+
+    // props.bpm の変更を検知して、localBpm.value を新しい値で更新
+    watch(() => props.bpm, (newBpm) => {
+      localBpm.value = newBpm;
+    });
+    watch(() => props.white, (newWhite) => {
+      localWhite.value = newWhite;
+    });
+    watch(() => props.white, (newGreen) => {
+      localWhite.value = newGreen;
+    });
+
+    // setup関数から返すオブジェクト。これにより、コンポーネント内で使用できる変数や関数を提供
+    return {
+      localBpm,localWhite,localGreen,
     };
   },
   methods: {
@@ -54,9 +99,9 @@ export default {
     }
   },
   watch: {
-    bpm: 'calcHS',
-    white: 'calcHS',
-    green: 'calcHS',
+    localBpm: 'calcHS',
+    localWhite: 'calcHS',
+    localGreen: 'calcHS',
   },
   mounted() {
     // ページが表示されたときにハイスピを計算
